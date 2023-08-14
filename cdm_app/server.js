beb8ede156ad08d73e1d144dfb67ea82bda2b70d
penguisbad <108484338+penguisbad@users.noreply.github.com>
2023-08-14 17:53:31 -0400

    Add files via upload

--- cdm_app/server.js
+++ cdm_app/server.js
@@ -27,27 +27,23 @@ const mapProduct = async (product) => {
   let response = await query('Which of the five NIST framework functions does ' + product +' perform? Answer "Identify", "Protect", "Detect", "Respond" or "Recover" and choose only one answer.', 0);
   let textResponse = response.choices[0].message.content;
   let xAxis = "";
-  console.log(response, textResponse);
   ["Identify", "Protect", "Detect", "Respond", "Recover"].forEach(element => {
     if (textResponse.includes(element)) {
       xAxis = element;
     }
   });
   response = await query('Explain very briefly how ' + product + ' performs the "' + xAxis + '" of the NIST framework', 0.3);
-  console.log(response)
   let xAxisExplanation = response.choices[0].message.content;
 
   response = await query('What does ' + product + ' protect? Answer "Devices", "Applications", "Networks", "Data", or "Users" and choose only one answer.', 0);
   textResponse = response.choices[0].message.content;
   let yAxis = "";
-  console.log(response, textResponse);
   ["Devices", "Applications", "Networks", "Data", "Users"].forEach(element => {
     if (textResponse.includes(element)) {
       yAxis = element;
     }
   });
   response = await query('Explain very briefly how ' + product + ' protects ' + yAxis, 0.3);
-  console.log(response);
   let yAxisExplanation = response.choices[0].message.content;
 
   return [[xAxis, yAxis], [xAxisExplanation, yAxisExplanation]];
