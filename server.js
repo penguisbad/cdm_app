351a1499956dd126bccd57d9f533b1a6d828e7ed
penguisbad <108484338+penguisbad@users.noreply.github.com>
2023-09-03 18:33:03 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,41 +1,46 @@
 /*
-
+- get description of product
+- turn it into embeddings
+- query pinecone for category
+- ask chatgpt based on the book where it is on the cyber defense matrix
 */
 
 const pinecone = require("@pinecone-database/pinecone");
 const cheerio = require("cheerio");
+const HTMLParser = require("node-html-parser");
 const express = require("express");
-const langchain = require("langchain");
+const lcOpenAI = require("langchain/chat_models/openai");
+const lcTextSplitter = require("langchain/text_splitter");
+const lcEmbeddings = require("langchain/embeddings/openai");
+const lcMemory = require("langchain/vectorstores/memory");
+const lcChains = require("langchain/chains");
 const request = require("request");
 const fs = require("fs");
 
-const openAIKey = process.env.OEPN_AI_KEY;
 const pineconeKey = process.env.PINECONE_API_KEY;
+const openAIKey = process.env.OPENAI_API_KEY;
 
 const pineconeClient = new pinecone.PineconeClient();
 pineconeClient.init({
     environment: "gcp-starter",
     apiKey: pineconeKey
 })
-const llm = new langchain.OpenAI({
-    temperature: 0
-});
-const text = fs.readFileSync("test.txt", "utf8");
-const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000 });
-const docs = await textSplitter.createDocuments([text]);
+const llm = new lcOpenAI.ChatOpenAI({ modelName: "gpt-3.5-turbo", temperature: 0 });
 
-const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
+//let docs, vectorStore, vectorStoreRetriever, chain;
 
-const vectorStoreRetriever = vectorStore.asRetriever();
-
-const chain = RetrievalQAChain.fromLLM(llm, vectorStoreRetriever);
+//const text = fs.readFileSync("cdm_book.txt", "utf8");
+//const textSplitter = new lcTextSplitter.RecursiveCharacterTextSplitter({ chunkSize: 500 });
 
 const askChatGPT = async (prompt) => {
     const response = await llm.predict(prompt);
     return response;
 }
 
-const mapProduct = (input)
+const askChatGPTbasedOnBook = async (prompt) => {
+    const response = await chain.call({ query: prompt });
+    return response;
+}
 
 const getDescription = (url) => {
     let description;
@@ -51,7 +56,6 @@ const getDescription = (url) => {
     });
     return description;
 }
-
 
 
 const getEmbeddings = async (input) => {
@@ -70,13 +74,43 @@ const getEmbeddings = async (input) => {
 }
 
 const queryIndex = async (vector) => {
-    const index = pineconeClient.Index("cdm-index");
+    const index = pineconeClient.Index("categories-and-descriptions");
     const queryRequest = {
         vector: vector,
-        topK: 5,
+        topK: 1,
         includeValues: true,
         includeMetadata: true
     };
     const response = await index.query({ queryRequest });
     return response;
 }
+
+const mapProduct = (input) => {
+    
+}
+
+const go = async () => {
+    const app = express();
+    app.get("/app", (req, res) => {
+        fs.readFile("app.html", "utf8", async (error, pageHTML) => {
+            if (req.query.product == null) {
+              res.send(pageHTML);
+              return;
+            }
+            if (error) {
+              console.log(error);
+              return;
+            }
+
+            const embedding = await getEmbeddings(req.query.product);
+            const response = await queryIndex(embedding["data"][0]["embedding"]);
+            
+            let root = HTMLParser.parse(pageHTML);
+            root.getElementById("explanation").innerHTML = response.matches[0].metadata.category;
+            res.send(root.toString());
+          });
+    });
+    app.listen(8080);
+}
+
+go();
